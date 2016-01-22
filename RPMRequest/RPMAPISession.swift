8d0d83f457f15e355d34eb204e6493350f2e90c9
Kang Lu <kang.lu@pvelocity.com>
2016-01-22 17:29:41 -0500

    First swift class implementation.

---
+++ RPMRequest/RPMAPISession.swift
@@ -0,0 +1,161 @@
+//
+//  RPMAPISession.swift
+//  RPMRequest
+//
+//  Created by Kang Lu on 2016-01-22.
+//  Copyright © 2016 Kang Lu. All rights reserved.
+//
+
+import Foundation
+
+public enum PVRequestParam
+{
+    case Element(attr: String, value: String)
+    indirect case NamedCollection(attr: String, params: [PVRequestParam])
+    indirect case Collection(params: [PVRequestParam])
+}
+
+public enum PVRequest
+{
+    case Operation(String, PVRequestParam)
+}
+
+public class RPMAPISession
+{
+    var cookie: String?
+
+    let hostURL: String
+    
+    let concurrentQueue : NSOperationQueue
+    let urlSessionConfig : NSURLSessionConfiguration
+    let urlSession : NSURLSession
+    
+    init(hostURL: String) {
+        self.concurrentQueue = NSOperationQueue()
+        self.concurrentQueue.maxConcurrentOperationCount = NSOperationQueueDefaultMaxConcurrentOperationCount
+        self.concurrentQueue.qualityOfService = NSQualityOfService.Background
+        
+        self.urlSessionConfig = NSURLSessionConfiguration.ephemeralSessionConfiguration()
+        self.urlSession = NSURLSession(configuration: self.urlSessionConfig, delegate: nil, delegateQueue: self.concurrentQueue)
+        
+        self.cookie = nil
+        self.hostURL = hostURL + "/PE/RPM"
+    }
+    
+    private func requestInXML (request request: PVRequest) -> String
+    {
+        
+        let emitElem = {
+            (attr: String, value: String) -> String in
+            var collection = ""
+            collection += "<\(attr)>"
+            collection += value
+            collection += "</\(attr)>"
+            return collection
+        }
+        
+        func expandParams (params params: PVRequestParam) -> String
+        {
+            let emitArray = {
+                (elements: [PVRequestParam]) -> String in
+                var collection = ""
+                for elem in elements {
+                    collection += expandParams(params: elem)
+                }
+                return collection
+            }
+            
+            switch params
+            {
+            case .Element(let attr, let value):
+                return emitElem(attr, value)
+                
+            case .Collection(let elements):
+                return emitArray(elements)
+                
+            case.NamedCollection(let attr, let elements):
+                return emitElem(attr, emitArray(elements))
+            }
+        }
+        
+        var xml: String = ""
+        xml += "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+        xml += "<PVRequest xmlns=\"http://pvelocity.com/rpm/pvrequest\">"
+        xml += "<Operation>"
+        
+        switch request
+        {
+        case .Operation(let name, let params):
+            xml += emitElem("Name", name)
+            xml += emitElem("Params", expandParams(params: params))
+        }
+        
+        xml += "</Operation>"
+        xml += "</PVRequest>"
+        
+        return (xml as NSString).stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet()) as String!
+    }
+    
+    private func createRequest(requestObj: PVRequest) -> NSURLRequest
+    {
+        let url = NSURL(string: self.hostURL)
+        let request = NSMutableURLRequest(URL: url!)
+        
+        let requestData = NSString(format: "dataformat=json&request=%@", requestInXML(request: requestObj))
+        
+        // Sample post
+        request.HTTPMethod = "POST"
+        request.setValue("Keep-Alive", forHTTPHeaderField: "Connection")
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField:"Content-type");
+        request.addValue("3.4", forHTTPHeaderField: "X-PVClient-Version")
+        request.addValue("iOS", forHTTPHeaderField: "X-PVClient-Platform")
+        request.setValue(self.cookie ?? "", forHTTPHeaderField: "Cookie")
+        
+        request.HTTPBody = (requestData as NSString).dataUsingEncoding(NSUTF8StringEncoding)
+        request.setValue(NSString(format: "%lu", request.HTTPBody!.length) as String, forHTTPHeaderField: "Content-Length")
+        request.HTTPShouldHandleCookies = false
+        
+        return request
+    }
+    
+    public func sendRequest(pvRequestObj: PVRequest) -> NSURLSessionTask
+    {
+        let request = createRequest(pvRequestObj)
+        let task = self.urlSession.dataTaskWithRequest(request, completionHandler: { (data, response, error) -> Void in
+            
+            if let _ = error {
+                // handle error
+            }
+            else
+            {
+                if let d = data {
+                    do {
+                        let obj = try NSJSONSerialization.JSONObjectWithData(d, options:NSJSONReadingOptions.AllowFragments)
+                        self.cookie = (response as! NSHTTPURLResponse).allHeaderFields["Set-Cookie"] as? String
+                        print(obj)
+                        print(self.cookie)
+                    }
+                    catch let error as NSError {
+                        print(error.localizedDescription)
+                        print("original content:\n\(String(data: d, encoding: NSUTF8StringEncoding)!)")
+                    }
+                }
+            }
+        })
+        
+        task.resume()
+        
+        return task
+    }
+    
+    public func awaitCompletion() -> Void
+    {
+        NSRunLoop.mainRunLoop().runUntilDate(NSDate(timeIntervalSinceNow: 2.0))
+        
+        print("waiting for queue to empty...")
+        concurrentQueue.waitUntilAllOperationsAreFinished()
+        
+        print("sleeping again...")
+    }
+    
+}
--- RPMRequest/main.swift
+++ RPMRequest/main.swift
@@ -11,80 +11,26 @@ import Foundation
 let numArgs = Process.arguments.count
 let progname = NSURL.fileURLWithPath(Process.arguments[0]).lastPathComponent
 
-var done = false
-
-func createRequest(urlString: String) -> NSURLRequest
-{
-    let url = NSURL(string: urlString)
-    let request = NSMutableURLRequest(URL: url!)
-    
-    let requestOp = [
-        "<?xml version=\"1.0\" encoding=\"utf-8\"?>",
-        "<PVRequest xmlns=\"http://pvelocity.com/rpm/pvrequest\">",
-        "<Operation>",
-        "<Name>Login</Name>",
-        "<Params>",
-        "<User>Admin</User>",
-        "<Password>psa</Password>",
-        "</Params>",
-        "</Operation>",
-        "</PVRequest>"
-    ]
-    
-    let requestData = NSString(format: "dataformat=json&request=%@", (requestOp.joinWithSeparator("") as NSString).stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!)
-    
-    // Sample post
-    request.HTTPMethod = "POST"
-    request.setValue("Keep-Alive", forHTTPHeaderField: "Connection")
-    request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField:"Content-type");
-    request.addValue("3.4", forHTTPHeaderField: "X-PVClient-Version")
-    request.addValue("iOS", forHTTPHeaderField: "X-PVClient-Platform")
-    request.setValue("", forHTTPHeaderField: "Cookie")
-    
-    request.HTTPBody = (requestData as NSString).dataUsingEncoding(NSUTF8StringEncoding)
-    request.setValue(NSString(format: "%lu", request.HTTPBody!.length) as String, forHTTPHeaderField: "Content-Length")
-    request.HTTPShouldHandleCookies = false
-    
-    return request
-}
-
-func sendRequest(urlString: String) -> NSURLSessionTask
-{
-    let sampleConfig = NSURLSessionConfiguration.ephemeralSessionConfiguration()
-    let sampleSession = NSURLSession(configuration: sampleConfig, delegate: nil, delegateQueue: NSOperationQueue.mainQueue())
-    
-    let request = createRequest(urlString)
-    let task = sampleSession.dataTaskWithRequest(request, completionHandler: { (data, response, error) -> Void in
-        
-        if let _ = error {
-            // handle error
-        }
-        else
-        {
-            if let d = data {
-                let obj = try! NSJSONSerialization.JSONObjectWithData(d, options:NSJSONReadingOptions.AllowFragments)
-                print(obj)
-            }
-        }
-        
-        done = true
-        
-    })
-    
-    task.resume()
-    
-    return task
-}
-
 if numArgs != 4
 {
     print ("Usage \(progname!): filename")
     
-    let task = sendRequest("http://goliath.pvelocity.com/PE/RPM")
+    let loginReq =  PVRequest.Operation("Login",
+        PVRequestParam.Collection(
+            params: [
+                PVRequestParam.Element(attr: "User", value: "Admin"),
+                PVRequestParam.Element(attr: "Password", value: "psa")
+            ]
+        ))
     
-    while(!done) {
-        NSRunLoop.mainRunLoop().runUntilDate(NSDate(timeIntervalSinceNow: 0.01))
+    let rpm = RPMAPISession(hostURL: "http://goliath.pvelocity.com")
+    
+    for i in 0..<1
+    {
+        let task = rpm.sendRequest(loginReq)
     }
+    
+    rpm.awaitCompletion()
 }
 else
 {
